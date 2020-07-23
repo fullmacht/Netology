d56fd11c6e741662609e3dbec5aa186cf4f4be2f
laptop <fany012@yandex.ru>
2020-07-23 18:41:46 +0300

    ДЗ по 'Улучшению качества модели'

--- Homework/1/boston.py
+++ Homework/1/boston.py
@@ -1,37 +1,33 @@
 from sklearn.datasets import load_boston
 from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-import numpy as np
-import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+
+
 X, y = load_boston(return_X_y=True)
-y = pd.DataFrame(y)
-X = pd.DataFrame(X)
-print(y.shape)
-print(X.shape)
-# print(np.median(y))
-le = LabelEncoder()
-y_pre = le.fit_transform(y)
-# ohe = OneHotEncoder()
-# y_pre = ohe.fit_transform(y)
-print(y_pre)
-grid_param = {
-    'n_neighbors':range(1,26),
-    'leaf_size' : range(2,30,2)
-}
-gs = GridSearchCV(KNeighborsClassifier(),grid_param,scoring='roc_auc',n_jobs=-1,cv=5)
-gs.fit(X,y_pre)
+
+k_range = list(range(1, 31))
+grid_param = dict(n_neighbors=k_range,leaf_size=k_range)
+gs = GridSearchCV(KNeighborsRegressor(),param_grid=grid_param,scoring='neg_mean_squared_error',n_jobs=-1,cv=10)
+gs.fit(X,y)
 print(gs.best_score_)
-for k in gs.cv_results_:
-    print(k, ":", gs.cv_results_[k][0])
+
+
+
 grid_param = {
-    'criterion' : ['gini', 'entropy'],
-    'max_depth' : range(1,31)
+    'criterion' : ['mse', 'friedman_mse', 'mae'],
+    'max_depth' : k_range
 }
-gs = GridSearchCV(DecisionTreeClassifier(),grid_param,scoring='accuracy',n_jobs=-1, cv=6)
-gs.fit(X,y_pre)
+gs = GridSearchCV(DecisionTreeRegressor(),grid_param,scoring='neg_mean_squared_error',n_jobs=-1, cv=5)
+gs.fit(X,y)
+print(gs.best_score_)
+
+
+
+grid_param = {
+    'normalize':[True,False]
+}
+gs = GridSearchCV(LinearRegression(), grid_param, scoring='neg_mean_squared_error',n_jobs=-1, cv=5)
+gs.fit(X,y)
 print(gs.best_score_)
