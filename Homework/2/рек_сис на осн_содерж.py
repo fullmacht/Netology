59af0f70526410427b142f42fbf707fa837ea1c4
laptop <fany012@yandex.ru>
2020-08-03 13:07:24 +0300

    Рексис

--- Homework/2/рек_сис на осн_содерж.py
+++ Homework/2/рек_сис на осн_содерж.py
@@ -21,7 +21,19 @@ ratings = ratings.drop(['timestamp','userId'],axis=1)
 data = pd.merge(movies,tags, left_on='movieId',right_on='movieId')
 data = pd.merge(data,ratings, left_on='movieId',right_on='movieId')
 data = data.drop(['userId','timestamp'],axis=1)
-print(data.head())
+data.dropna(inplace=True)
+# print(data.head())
+
+med = data.groupby(['movieId']).rating.mean()
+data = pd.merge(data,med, left_on='movieId',right_on='movieId')
+data = data.drop('rating_x',axis=1)
+y = data['rating_y']
+x = data.drop('rating_y',axis=1)
+x = x.groupby(['movieId'])
+# print(x.head())
+# from sklearn.model_selection import train_test_split
+# x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+
 # def change_string(s):
 #     return ' '.join(s.replace(' ', '').replace('-', '').split('|'))
 #
@@ -29,7 +41,6 @@ print(data.head())
 
 # movies_with_tags = movies.join(tags.set_index('movieId'), on='movieId')
 # print(movies_with_tags.head())
-data.dropna(inplace=True)
 
 tag_strings = []
 movies = []
@@ -42,6 +53,11 @@ for movie, group in data.groupby('title'):
     gen()
     tag_strings.append(' '.join([str(s).replace(' ', '').replace('-', '') for s in group.tag.values]))
     movies.append(movie)
+
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+cv = CountVectorizer()
+genres_vec = cv.fit_transform(movie_genres)
+# print(genres_vec.shape)
 
 # print(movie_genres[:10])
 # print(tag_strings[:10])
