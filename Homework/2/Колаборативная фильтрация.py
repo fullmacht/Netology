f425d62a5b7c97104a639e980a9afc2326a9d974
laptop <fany012@yandex.ru>
2020-08-08 18:22:22 +0300

    Колаб фильтрация 2

--- Homework/2/Колаборативная фильтрация.py
+++ Homework/2/Колаборативная фильтрация.py
@@ -5,8 +5,8 @@ from surprise import accuracy
 from surprise import Reader
 from surprise.model_selection import train_test_split
 
-ratings =pd.read_csv(r'C:\Users\laptop\PycharmProjects\Netology\Classwork\data\big_movielens\ratings.csv')
-movies =pd.read_csv(r'C:\Users\laptop\PycharmProjects\Netology\Classwork\data\big_movielens\movies.csv')
+ratings =pd.read_csv(r'C:\Users\laptop\PycharmProjects\Netology\Classwork\data\ratings.csv')
+movies =pd.read_csv(r'C:\Users\laptop\PycharmProjects\Netology\Classwork\data\movies.csv')
 
 
 movies_with_ratings = movies.join(ratings.set_index('movieId'), on='movieId').reset_index(drop=True)
@@ -20,3 +20,16 @@ dataset = pd.DataFrame({
 
 print(ratings.rating.min())
 print(ratings.rating.max())
+
+reader = Reader(rating_scale=(0.5, 5.0))
+data = Dataset.load_from_df(dataset, reader)
+
+trainset, testset = train_test_split(data, test_size=.15,random_state=1)
+
+
+for i in range(1,101):
+    algo = KNNWithMeans(k=i, sim_options={'name': 'pearson_baseline', 'user_based': False})
+    algo.fit(trainset)
+    test_pred = algo.test(testset)
+    print(i,'-------------',accuracy.rmse(test_pred, verbose=True))
+#8861 -61
