8d18d8f8a3427471277dbeff13f818ccd6de3436
laptop <fany012@yandex.ru>
2020-08-28 16:32:20 +0300

    Добавил ДЗ по колаборативной фильтрации

--- Homework/rec_sis/Колаборативная фильтрация.py
+++ Homework/rec_sis/Колаборативная фильтрация.py
@@ -1,9 +1,10 @@
 import pandas as pd
-from surprise import KNNWithMeans,KNNBasic
+from surprise import KNNWithMeans
 from surprise import Dataset
 from surprise import accuracy
 from surprise import Reader
 from surprise.model_selection import train_test_split
+import operator
 
 ratings =pd.read_csv(r'C:\Users\laptop\PycharmProjects\Netology\Classwork\data\ratings.csv')
 movies =pd.read_csv(r'C:\Users\laptop\PycharmProjects\Netology\Classwork\data\movies.csv')
@@ -18,18 +19,24 @@ dataset = pd.DataFrame({
     'rating': movies_with_ratings.rating
 })
 
-print(ratings.rating.min())
-print(ratings.rating.max())
+min_r = ratings.rating.min()
+max_r = ratings.rating.max()
 
-reader = Reader(rating_scale=(0.5, 5.0))
+reader = Reader(rating_scale=(min_r, max_r))
 data = Dataset.load_from_df(dataset, reader)
 
 trainset, testset = train_test_split(data, test_size=.15,random_state=1)
 
-
-for i in range(1,101):
+d = {}
+for i in range(40,42):
     algo = KNNWithMeans(k=i, sim_options={'name': 'pearson_baseline', 'user_based': False})
     algo.fit(trainset)
     test_pred = algo.test(testset)
-    print(i,'-------------',accuracy.rmse(test_pred, verbose=True))
-#8861 -61
+    a = accuracy.rmse(test_pred, verbose=False)
+    d[i] = a
+    print('Завершено предсказание при k = {}'.format(i))
+mi = min(d.items(), key=operator.itemgetter(1))[0]
+print('Минимальная rmse = {} при k = {}'.format(d[mi],mi))
+
+#user_b Минимальный rmse = 0.88 при k = 61
+#item_b  Минимальная rmse = 0.8689299715492731 при k = 41
--- test.py
+++ test.py
@@ -1,48 +1,6 @@
-import pandas as pd
-import numpy as np
-
-# from tqdm.notebook import tqdm
-
-from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+import operator
+d = {1:2,2:4}
+m = (min(d.items(), key=operator.itemgetter(1))[0])
+print('min rmse{} - k{}'.format(d[m],m))
 
 
-desired_width=320
-pd.set_option('display.width', desired_width)
-# np.set_printoption(linewidth=desired_width)
-pd.set_option('display.max_columns',30)
-
-
-links = pd.read_csv(r'C:\Users\laptop\PycharmProjects\des_tree_clf_titanik\Classwork\data\links.csv')
-movies = pd.read_csv(r'C:\Users\laptop\PycharmProjects\des_tree_clf_titanik\Classwork\data\movies.csv')
-ratings = pd.read_csv(r'C:\Users\laptop\PycharmProjects\des_tree_clf_titanik\Classwork\data\ratings.csv')
-tags = pd.read_csv(r'C:\Users\laptop\PycharmProjects\des_tree_clf_titanik\Classwork\data\tags.csv')
-# print(ratings.head())
-ratings = ratings.drop(['timestamp','userId'],axis=1)
-data = pd.merge(movies,tags, left_on='movieId',right_on='movieId')
-data = pd.merge(data,ratings, left_on='movieId',right_on='movieId')
-data = data.drop(['userId','timestamp'],axis=1)
-print(data.head())
-# def change_string(s):
-#     return ' '.join(s.replace(' ', '').replace('-', '').split('|'))
-#
-# movie_genres = [change_string(g) for g in data.genres.values]
-
-# movies_with_tags = movies.join(tags.set_index('movieId'), on='movieId')
-# print(movies_with_tags.head())
-data.dropna(inplace=True)
-
-tag_strings = []
-movies = []
-movie_genres = []
-# print(data.head())
-def gen():
-    for k in data.genres.values:
-        movie_genres.append(' '.join(str(k).replace(' ', '').replace('-', '').split('|')))
-for movie, group in data.groupby('title'):
-    gen()
-    tag_strings.append(' '.join([str(s).replace(' ', '').replace('-', '') for s in group.tag.values]))
-    movies.append(movie)
-
-# print(movie_genres[:10])
-# print(tag_strings[:10])
-# print(movies[:10])
